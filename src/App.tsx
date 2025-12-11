435f2a9208d0ed5f4f535a410514465b5376da3d
[dyad] <popangame@gmail.com>
2025-12-11 14:10:08 +0700

    [dyad] Menghapus halaman SystemLookup dan UnifiedReportACS - wrote 2 file(s), deleted 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,12 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import UnifiedReportACS from "./pages/UnifiedReportACS";
-import Dashboard from "./pages/Dashboard"; // Import halaman baru
-import JobMaster from "./pages/JobMaster"; // Import halaman baru
-import ArtworkLog from "./pages/ArtworkLog"; // Import halaman baru
-import DesignerMaster from "./pages/DesignerMaster"; // Import halaman baru
-import ArtworkTypeMaster from "./pages/ArtworkTypeMaster"; // Import halaman baru
-import SystemLookup from "./pages/SystemLookup"; // Import halaman baru
-import Layout from "./components/Layout"; // Import komponen Layout
+import Dashboard from "./pages/Dashboard";
+import JobMaster from "./pages/JobMaster";
+import ArtworkLog from "./pages/ArtworkLog";
+import DesignerMaster from "./pages/DesignerMaster";
+import ArtworkTypeMaster from "./pages/ArtworkTypeMaster";
+import Layout from "./components/Layout";
 
 const queryClient = new QueryClient();
 
@@ -30,8 +28,6 @@ const App = () => (
             <Route path="/artwork-log" element={<ArtworkLog />} />
             <Route path="/designer-master" element={<DesignerMaster />} />
             <Route path="/artwork-type-master" element={<ArtworkTypeMaster />} />
-            <Route path="/system-lookup" element={<SystemLookup />} />
-            <Route path="/unified-report-acs" element={<UnifiedReportACS />} />
           </Route>
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
